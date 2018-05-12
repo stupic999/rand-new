40d35c9cd2957e84e38b54da1576f40d5aa945e6
DESKTOP-H468MRI\Moucy Cat <aierzhiguang2@gmail.com>
2018-05-12 22:14:27 +0800

    换网址～并加入注解

---
+++ rand2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rand2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            // 设定一个存放抽奖号码的list
+            List<int> number = new List<int>();
+            
+            // 把数字1-42加入number里
+            for (int i = 0; i < 42; i++)
+            {
+                number.Add(i);
+            }
+
+            // 抽取6个号码，并把抽出的号码从list移除，保证不重复
+            for (int i = 0; i <6; i++)
+            {
+                int j = rand.Next(1,number.Count);
+                Console.Write(number[j]+" ");
+                number.RemoveAt(j);
+            }
+            Console.ReadLine();
+        }
+    }
+}
